5c6f0f786a32abea0e1527591ab4458ba3c2f635
Carlo <carlo.ita98@gmail.com>
2021-05-12 01:47:52 +0200

    better structure with docker

--- graphmarket/libs/entities/product/Product.ts
+++ graphmarket/libs/entities/product/Product.ts
@@ -1,4 +1,5 @@
 import {
+  Column,
   CreateDateColumn,
   Entity,
   Index,
@@ -6,7 +7,7 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { Directive, Field, GraphQLTimestamp, ObjectType } from 'type-graphql';
-import { GraphQLID } from '@libs/graphql';
+import { GraphQLID, GraphQLNonEmptyString, GraphQLURL } from '@libs/graphql';
 
 @Entity('product')
 @ObjectType('Product')
@@ -14,14 +15,26 @@ import { GraphQLID } from '@libs/graphql';
 export class Product {
   @PrimaryGeneratedColumn('uuid')
   @Index()
-  @Field(() => GraphQLID)
+  @Field(() => GraphQLID, { description: `Product's identifier` })
   id!: string;
 
+  @Column({ length: 256 })
+  @Field(() => GraphQLNonEmptyString, { description: `Product's name` })
+  name!: string;
+
+  @Column({ length: 512, nullable: true, default: undefined })
+  @Field(() => GraphQLNonEmptyString, { nullable: true, description: `Product's description` })
+  description?: string;
+
+  @Column({ length: 512, nullable: true, default: undefined })
+  @Field(() => GraphQLURL, { description: `Product's image` })
+  image!: string;
+
   @CreateDateColumn({ name: 'created_at', type: 'timestamptz', update: false })
-  @Field(() => GraphQLTimestamp)
+  @Field(() => GraphQLTimestamp, { description: `Product's creation timestamp` })
   createdAt!: Date;
 
   @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
-  @Field(() => GraphQLTimestamp)
+  @Field(() => GraphQLTimestamp, { description: `Product's last updated timestamp` })
   updatedAt!: Date;
 }
