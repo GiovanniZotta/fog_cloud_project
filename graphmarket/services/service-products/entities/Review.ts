c07c344d338555dd9f639319c5c7770a206a7b0d
Carlo <carlo.ita98@gmail.com>
2021-05-13 19:27:54 +0200

    ok for product

--- graphmarket/services/service-products/entities/Review.ts
+++ graphmarket/services/service-products/entities/Review.ts
@@ -1,12 +1,16 @@
 import { Directive, Field, ObjectType } from 'type-graphql';
 import { GraphQLID } from '@libs/graphql';
-import { Review } from './Review';
+import { Product, Review as ReviewBase } from '@libs/entities';
 
 @ObjectType('Review')
 @Directive(`@key(fields: "id")`)
 @Directive('@extends')
-export class ReviewReference implements Partial<Review> {
+export class Review implements Partial<ReviewBase> {
   @Field(() => GraphQLID)
   @Directive('@external')
   id!: string;
+
+  @Field(() => Product)
+  @Directive(`@requires(fields: "productId")`)
+  product!: Product;
 }
