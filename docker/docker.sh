97e8e0e254b67848695e745801c17bbf01e1200a
Carlo <carlo.ita98@gmail.com>
2021-05-18 20:33:29 +0200

    moved docker database into folder

--- docker/docker.sh
+++ docker/docker.sh
@@ -31,7 +31,7 @@ fi
 
 # Images
 images_name=('database' 'gateway' 'service-products' 'service-reviews')
-images_file=('Dockerfile.database' 'Dockerfile.gateway' 'Dockerfile.service.products' 'Dockerfile.service.reviews')
+images_file=('database/Dockerfile.database' 'Dockerfile.gateway' 'Dockerfile.service.products' 'Dockerfile.service.reviews')
 # END Images
 
 for ((i = 0; i < ${#images_name[@]}; ++i)); do
